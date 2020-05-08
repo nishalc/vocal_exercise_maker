12efbab6a72bfae277812638aad0109a964d5495
Nishal <nishalc@outlook.com>
2020-05-08 15:04:13 +0100

    updated exe

--- produce_exe.py
+++ produce_exe.py
@@ -4,13 +4,11 @@ import shutil
 PyInstaller.__main__.run([
    'main.py',
    '--name=%s' % 'vocal_exercise_maker',
-   '--onefile',
    '--windowed'
 ])
 
-#source = 'dist/vocal_exercise_maker.exe'
-#destination = 'vocal_exercise_maker.exe'
+source = 'dist/vocal_exercise_maker.exe'
+destination = 'vocal_exercise_maker.exe'
 
-#dest = shutil.move(source, destination)
-
+dest = shutil.move(source, destination)
 
